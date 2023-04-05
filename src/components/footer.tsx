596407638ee7a4c3964f0882b92cd2ed2837a7e8
ProjectStuffUwU <projectstuff10.01@gmail.com>
2023-04-05 18:22:44 +0530

    Made changes to navbar,footer added shopByProductGrid component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
 
-        <footer className="bg-secondary dark:bg-gray-900">
+        <footer className="bg-secondary dark:bg-gray-900 text-sm">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
@@ -10,7 +10,7 @@ export default function Footer() {
                             {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">JWC</span> */}
                         </a>
                     </div>
-                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div className="grid grid-cols-2 gap-8 ml-96 sm:gap-6 justify-items-center">
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Address</h2>
                             <ul className="text-gray-600 dark:text-gray-400 font-medium">
